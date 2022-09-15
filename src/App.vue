693ecb7dfea308ec94739ff2877def2b938a2036
Vaidas <vaidas.makstutis@gmail.com>
2022-09-15 19:51:11 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <message>Hello There</message>
+    <message>Hello There again</message>
+    <message>Hello There again again</message>
+  </div>
+</template>
+
+<script>
+import Message from "./components/Message.vue";
+
+export default {
+  name: "app",
+  components: { Message },
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
